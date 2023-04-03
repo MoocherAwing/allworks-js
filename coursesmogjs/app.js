51ae52f8fbac741d45b5368097782b246a441da6
MoocherAwing <raul.hasanau@gmail.com>
2023-04-03 15:24:29 +0300

    next commit2

--- coursesmogjs/app.js
+++ coursesmogjs/app.js
@@ -51,67 +51,67 @@
 
 // console.log (2 && 1 && (3 || 2) || null && (undefined || 1));
 
-// const enterNum = prompt('enter');
-// if (1 || 3) {
-//     console.log ('1');
-// } else if (2 && 5) {
-//     console.log ('2');
-// }
+const enterNum = prompt('enter');
+if (1 || 3) {
+    console.log ('1');
+} else if (2 && 5) {
+    console.log ('2');
+}
 
-// const getYourNick = confirm('Are you admin?');
-// const getYourPassword = +prompt('get Your Password');
+const getYourNick = confirm('Are you admin?');
+const getYourPassword = +prompt('get Your Password');
 
-// if (getYourNick) {
-//     const getYourPassword = +prompt('enter password');
-//     if (getYourPassword === 'admin') {
-//         alert('admin');
-//     } else {
-//         alert('You are not admin');
-//     }
-// } else {
-//     console.log(alert('You are not admin'));
-// }
+if (getYourNick) {
+    const getYourPassword = +prompt('enter password');
+    if (getYourPassword === 'admin') {
+        alert('admin');
+    } else {
+        alert('You are not admin');
+    }
+} else {
+    console.log(alert('You are not admin'));
+}
 
-// const getYourAge = prompt ('How old are you?');
+const getYourAge = prompt ('How old are you?');
 
-// if (getYourAge < 18){
-//     alert('young');
-// } else if (getYourAge > 18 && getYourAge < 51) {
-//     alert('middle');
-// } else if (getYourAge > 50 && getYourAge < 101) {
-//     alert('old');
-// }
+if (getYourAge < 18){
+    alert('young');
+} else if (getYourAge > 18 && getYourAge < 51) {
+    alert('middle');
+} else if (getYourAge > 50 && getYourAge < 101) {
+    alert('old');
+}
 
-// let num = 0;
-// while (num < 5) {
-//     num++;
-//     console.log(num);
-// }
+let num = 0;
+while (num < 5) {
+    num++;
+    console.log(num);
+}
 
-// let num1 = 0;
-// do {
-//     num1++;
-//     console.log(num1);
-// } while (num1 < 5);
+let num1 = 0;
+do {
+    num1++;
+    console.log(num1);
+} while (num1 < 5);
 
-// for (let i = 0; i <= 5; i++) {
-//     console.log(i);
-//     if(i === 2){
-//         break;
-//     }
-// }
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+    if(i === 2){
+        break;
+    }
+}
 
-// for (let i = 0; i<=5; i++){
-//     if (i % 2 === 0 && i != 0){
-//         console.log(`${i} - четное число`);
-//     }
-//     else if (i === 0){
-//         console.log (`${i} - это ноль`);
-//     }
-//     else {
-//         console.log(`${i} - нечетное число`);
-//     }
-// }
+for (let i = 0; i<=5; i++){
+    if (i % 2 === 0 && i != 0){
+        console.log(`${i} - четное число`);
+    }
+    else if (i === 0){
+        console.log (`${i} - это ноль`);
+    }
+    else {
+        console.log(`${i} - нечетное число`);
+    }
+}
 
 let str = "abbbabaa";
 // console.log (str.length);
